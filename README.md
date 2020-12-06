1c484acc8ac7f4edde90de89768a84c6379942aa
saturbasement <tiago.satur@basement.io>
2020-12-06 17:42:48 -0300

    Add node http server

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # node-bookmark-app
+
+Build a http server - serving `/public/index.html`
+
+Consume api in the frontend
+
+## How start the app
+
+```js
+yarn start
+```
+
+Access [localhost:5000](http://localhost:5000/)
+
+## Curiosity
+
+npm init -y (-y: without questions)
---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+
+http.createServer((req, res) => {
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+    const fileExtention = path.extname(filePath)
+    const allowdFilePaths = ['.html', '.css', '.js']
+    const allowed = allowdFilePaths.find((item) => item === fileExtention)
+
+    // In case a file doens't exist, it does not freeze the app (like favicon.ico)
+    if(!allowed) return
+
+    fs.readFile(
+        filePath,
+        (error, content) => {
+            if(error) throw error
+
+            res.end(content)
+        }
+        )    
+    
+}).listen(5000)
